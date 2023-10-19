993366b3bb48f5b6ce975340a9721648f2291a1f
teenas-code <teenagithub@gmail.com>
2023-10-18 21:26:15 -0400

    align with STAR

--- star_alignment/build_ref.sh
+++ star_alignment/build_ref.sh
@@ -3,4 +3,4 @@
 
 # buiding refernce database using STAR with genome fa and gff downloaded from NCBI for cereibacter sphaeroides
 
-STAR --runMode genomeGenerate --genomeDir ref/ --genomeFastaFiles ~/Documents/RNA_seq_workflow/star_alignment/ncbi_dataset/data/GCA_000021005.1/GCA_000021005.1_ASM2100v1_genomic.fna --sjdbGTFfile ~/Documents/RNA_seq_workflow/star_alignment/ncbi_dataset/data/GCA_000021005.1/genomic.gtf --runThreadN 1 
+STAR --runMode genomeGenerate --genomeDir ref/ --genomeFastaFiles ./ncbi_dataset/data/GCF_000021005.1/GCF_000021005.1_ASM2100v1_genomic.fna --sjdbGTFfile ./ncbi_dataset/data/GCF_000021005.1/genomic.gff --runThreadN 1 --genomeSAindexNbases 8 --sjdbGTFtagExonParentTranscript Parent --sjdbGTFtagExonParentGene ID
--- star_alignment/get_genome.sh
+++ star_alignment/get_genome.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# get_genome.sh
-
-
-# command to get the reference genome for rhodobacter sheroides
-
 curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000021005.1/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000021005.1.zip" -H "Accept: application/zip"
